d0ac1ca2133407d20a4892c56264c403e5a1c255
leslie wilson <lesliewilson@Leslies-MacBook-Pro.local>
2018-02-13 23:33:00 -0500

    excercises

--- chapter-four/Chapt4.py
+++ chapter-four/Chapt4.py
@@ -52,3 +52,23 @@ def main():
 main ()
 
 # indexing, slicing and concatenating are combined
+
+#month.py
+    #a program to print the abbreviation of a month, given its numbers
+
+def main():
+    #months is used as a lookup table
+    months = "janfebmaraprmayjunjulaugsepoctnovdec"
+
+    n = input("enter a month number(1-12): ")
+
+    #  compute starting position of month n in months
+     pos = (n-1) * 3
+
+    #  grab the appropriate slice from months
+    monthAbbrev = months [pos:pos+3]
+
+    #print result
+    print ?the month abbreviation is" monthAbbrev + "."
+
+    main()
