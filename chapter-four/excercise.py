efa81bf896e1e05e751419d02cac0fada1566eec
leslie wilson <lesliewilson@Leslies-MacBook-Pro.local>
2018-02-18 15:32:57 -0500

    more excercises

---
+++ chapter-four/excercise.py
@@ -0,0 +1,13 @@
+'''open text.txt - count the amount of characters and the total amount of words and print it to the console.'''
+
+# example, pg 109 ZELLE
+# printfile.py
+# prints file to screen
+
+def main():
+        fname = raw_input("enter filename: ")
+        infile = open(fname,'r')
+        data + infile.read()
+        print data
+
+main()
--- chapter-four/extra_excercises.py
+++ chapter-four/extra_excercises.py
@@ -1,6 +1,6 @@
 # ZELLE, PAGE 120, PROBLEM 10
 
-# write a program that counts the number of words in a sentence entered by the user
+# write a program that counts the number of wordgit s in a sentence entered by the user
 
 # this is a program that takes an input and prints out the number of words in that input
 
@@ -14,6 +14,48 @@ print("the number of words in the sentance is: " + str(count))
 
 # TERMINAL OUTPUT:
 
-# ➜  chapter-four git:(moreChapt4) ✗ python extra_excercises.py
+
 # please enter a sentance: happy me!!
 # the number of words in the sentance is: 2
+# _______
+
+# PROBLEM 11
+
+# write a program that calculates the average word
+# length in a sentance entered by the user
+# def averageLength (s, words):
+#     sum = 0
+#     for word in words:
+#     s = raw_input("please enter a sentance: ")
+# words = s.split()
+# length = len(words)
+# average = sum(length)
+#
+# print(average)
+
+
+def averageLength():
+    s = raw_input("please enter a sentence: ")
+    arrayofwords = s.split()
+    sum = 0
+    for word in arrayofwords:
+        sum += len(word)
+    average = sum / len(arrayofwords)
+    print average
+
+averageLength()
+
+
+# average = sum(len(words)/ len(words))
+
+
+
+
+# did not work, TERMINAL OUTPUT
+
+# >>> s = raw_input("please enter a sentance: ")
+# please enter a sentance: pretty
+# >>> words = s.split()
+# >>> average = sum(len(word)) for word in words /
+  # File "<stdin>", line 1
+    # average = sum(len(word)) for word in words /
