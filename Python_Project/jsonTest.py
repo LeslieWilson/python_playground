151fd7daa6269e20e391c85c5160dcf128f51b3f
leslie wilson <lesliewilson@Leslies-MacBook-Pro.local>
2018-03-17 13:44:32 -0400

    test

---
+++ Python_Project/jsonTest.py
@@ -0,0 +1,13 @@
+import json
+
+with open ('input.json') as json_data:
+    data = json.load(json_data)
+
+    for r in data['Employee']:
+        fo = open(r['id'] + "_" + r['name']+ ".txt", "wb")
+
+        fo.write(r(['name']+['manager'])
+
+        fo.close()
+
+        print r['name']
