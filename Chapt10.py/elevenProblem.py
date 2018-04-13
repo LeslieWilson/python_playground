68ce12a0514a0b2ef2aaa808b15df3d871da5c90
leslie wilson <lesliewilson@Leslies-MacBook-Pro.local>
2018-04-12 21:43:08 -0400

    annotated

--- Chapt10.py/elevenProblem.py
+++ Chapt10.py/elevenProblem.py
@@ -1,14 +1,26 @@
 
+
+"""
+Leslie Wilson
+April 12 2018
+elevenProblem.py
+"""
+
 from random import *
 
 
 class Card(object):
+
+    """creates card object with a rank and suit"""
+
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
 
 
     def getrank(self):
+        """deliniates the rank of a card depending on what number is randomly chosen """
+
         if self.rank == 1:
             return "ace"
         elif self.rank == 11:
@@ -21,6 +33,9 @@ class Card(object):
             return " %d" % (self.rank)
 
     def getSuit(self):
+
+        """deliniates the suit of a card depending on what letter is randomly chosen """
+
         if self.suit == "d":
             return  "diamonds"
         elif self.suit == "c":
@@ -32,6 +47,9 @@ class Card(object):
 
 
     def BJValue(self):
+
+        """describes bj value depending on what number is randomly chosen"""
+
         if self.rank == 1:
             return "bj value 1"
         elif self.rank == 11:
@@ -50,28 +68,26 @@ class Card(object):
 card = Card(randrange(1,13), choice(["s","d","c","h"]))
 
 def randomCard():
+    """makes array of randomly selected cards and their values denoted above. also lets user choose how many cards to draw."""
+
     array = []
 
-    input = raw_input("put your number in: ")
+    input = raw_input("how many cards do you want to use: ")
     for i in range(int(input)):
         card = Card(randrange(1,13),
         choice(["s","d","c","h"]))
         array.append(card)
     return array
 
-# x = randomCard()
-#
-# for card in x:
-#     print(card)
-    # print(card.BJValue())
 
 
+def main():
+    x = randomCard()
+    for card in x:
+        print(card)
+        print(card.BJValue())
 
-
-
-
-
-
-card.BJValue()
-card.getrank()
-card.getSuit()
+    card.BJValue()
+    card.getrank()
+    card.getSuit()
+main()
