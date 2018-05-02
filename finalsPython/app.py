367cc93a34b72527947e9a2bfc770acf2e4730a2
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-05-02 12:09:27 -0400

    made flashcard app work

--- finalsPython/app.py
+++ finalsPython/app.py
@@ -1,7 +1,11 @@
-# this will be a flashcard app that quizzes you on stored flashcards or allows you to create new flashcards.
+# Final Python Project
 
-# command prompt interface, asks you if you want to make a new flashcard or go through saved flashcards
-
-# if you make a new flash card - it asks for the question - saves it to a text file - then it asks for the answer and saves it to the same text file, with a comma or a new line after the question
-
-# then if you ask to go through the flashcards it types when out and tells you to type a for the answer or something, it reads the first line of the text file for the question, then reads the next line for the answer and keeps looping through the file
+# For my final python project, I want to make a flashcard app that quizzes you on stored flashcards or allows you to create new flashcards, and uses regex to approximate how well you answered.
+#
+# The command prompt interface will ask a user if they want to make a new flashcard or go through saved flashcards
+#
+# If the user makes a new flash card - it asks for the question - saves it to a text file - then it asks for the answer and saves it to the same text file, with a comma or a new line after the question
+#
+# If the user asks to go through the flashcards, the program will prompt with a question and the user will type in as close an answer as they can. if the answers match closely enough (using regex) it will show you the answer and move on to the next card. If the users answer does not match the real answer closely enough, it tells you to try again or offers to show you the answer.
+#
+# So somewhere in the object there will be a method called "readtestquestion". This method will read the first line of the text file for the question, then read the next line for the answer and keep looping through the file. This will show the question and answer successively.
